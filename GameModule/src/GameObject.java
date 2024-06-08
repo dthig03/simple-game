b7b4c292d44afd4660f9e15762f7bfeffbed6d30
Darren Thigpen <dthig0303@gmail.com>
2024-06-08 16:15:03 -0400

    Created Player object class, and added getters and setters to the GameObject class.

--- GameModule/src/GameObject.java
+++ GameModule/src/GameObject.java
@@ -12,4 +12,44 @@ public abstract class GameObject {
     }
     public abstract void tick();
     public abstract void render(Graphics g);
+
+    public int getSpeedX() {
+        return speedX;
+    }
+
+    public void setSpeedX(int speedX) {
+        this.speedX = speedX;
+    }
+
+    public int getSpeedY() {
+        return speedY;
+    }
+
+    public void setSpeedY(int speedY) {
+        this.speedY = speedY;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public ID getId() {
+        return id;
+    }
+
+    public void setId(ID id) {
+        this.id = id;
+    }
 }
---
+++ GameModule/src/Player.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+
+public class Player extends GameObject{
+
+    public Player(int x, int y, ID id) {
+        super(x, y, id);
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+
+    }
+}
